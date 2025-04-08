a0789f2262f622b4499277c4d7fa701ebdf00e3c
Hao Nghiem <qtvhao@gmail.com>
2025-04-08 23:26:16 +0700

    Add

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { YoutubeStudio } from "./YoutubeStudio.js";
 
-(async () => {
-    const studio = new YoutubeStudio();
-    await studio.start();
-    await studio.fetchAndSaveImpressionsByContentPage();
-})();
+export {
+    YoutubeStudio,
+}
